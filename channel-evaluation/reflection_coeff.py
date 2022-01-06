- Add Box method that plots the reflection coefficient of each element for each configuration. - Correct bug involving computation of phase shifts for BSs and UEs. - Figure computing average number of collisions

channel-evaluation/reflection_coeff.py
@@ -84,7 +84,8 @@ omega = 2*np.pi/wavelengh   # wavenumber
 theta_i = np.radians(45)
 
 # Desired direction of reflection
-configs = np.radians(np.array([30, 45, 60]))
+#configs = np.radians(np.array([30, 45, 60]))
+configs = np.array([0.19634954, 0.58904862, 0.9817477])
 
 # Define x-axis of the surface: [-5\lambda,5\lambda] corresponds to the case
 # that the surface is 10\lambda long along the x-axis

environment/box.py
@@ -10,6 +10,42 @@ from matplotlib import rc
 from scipy.constants import speed_of_light
 
 
+def quant(x, bits):
+    """
+    Quantize a signal x considering the given number of bits.
+
+    Parameters
+    ----------
+
+    x : array of floats
+        input signal
+
+    bits : integer
+        number of bits that defines the step size (resolution) of the
+        quantization process.
+
+    Returns
+    -------
+
+    yk : array of floats
+        quantized version of the input signal
+    """
+
+    # Obtain step size
+    Delta = (1 / 2) ** (bits - 1)
+
+    # Re-scale the signal
+    x_zeros = x * (1 - 1e-12)
+    x_scaled = x_zeros - Delta / 2
+
+    # Quantization stage
+    k = np.round(x / Delta) * Delta
+
+    # Reconstruction stage
+    yk = k + Delta / 2
+
+    return yk
+
 class Box:
     """Class Box creates an environment square box with specific parameters and nodes."""
     def __init__(self,
@@ -199,13 +235,13 @@ class Box:
         Omega_ue = self.bs.gain * self.ue.gain * self.ris.area**2 / (4 * np.pi * dist_bs * self.ris.num_els)**2
 
         # Common factor
-        common = Omega_ue / (dist_ue**2)
+        common_factor = Omega_ue / (dist_ue**2)
 
         # DL channel gains
-        channel_gains_dl = common * np.cos(self.bs.incidence_angle)**2
+        channel_gains_dl = common_factor * np.cos(self.bs.incidence_angle)**2
 
         # UL channel gains
-        channel_gains_ul = common * np.cos(self.ue.incidence_angle)**2
+        channel_gains_ul = common_factor * np.cos(self.ue.incidence_angle)**2
 
         # Compute distance BS-RIS-elements of shape (N,)
         dist_bs_el = np.linalg.norm(self.bs.pos - self.ris.pos_els, axis=-1)
@@ -214,10 +250,10 @@ class Box:
         dist_ue_el = np.linalg.norm(self.ue.pos[:, np.newaxis, :] - self.ris.pos_els, axis=-1)
 
         # BS phase shifts
-        phase_shifts_bs = 2 * np.pi * np.mod(dist_bs_el / self.wavenumber, 1)
+        phase_shifts_bs = 2 * np.pi * np.mod(dist_bs_el / self.wavelength, 1)
 
         # UE phase shifts
-        phase_shifts_ue = 2 * np.pi * np.mod(dist_ue_el / self.wavenumber, 1)
+        phase_shifts_ue = 2 * np.pi * np.mod(dist_ue_el / self.wavelength, 1)
 
         return channel_gains_dl, channel_gains_ul, phase_shifts_bs, phase_shifts_ue
 
@@ -232,20 +268,53 @@ class Box:
         None.
 
         """
+        # # Range over RIS horizontal dimension
+        # v_range = np.arange(-self.ris.size_h / 2, +self.ris.size_h / 2, step=1e-3)
+        #
+        # # Prepare to save local surface phase
+        # local_surface_phase = np.zeros((self.ris.num_configs, v_range.size))
+        #
+        # # Go through all configurations
+        # for config, theta_s in enumerate(self.ris.set_configs):
+        #     local_surface_phase[config, :] = 2 * np.pi * np.mod(self.wavenumber * (np.sin(self.bs.incidence_angle) - np.sin(theta_s)) * v_range, 1)
+        #
 
         # Prepare to save the reflection coefficients for each configuration
         reflection_coefficients_dl = np.zeros((self.ris.num_configs, self.ris.num_els))
 
         # Go through all configurations
         for config, theta_s in enumerate(self.ris.set_configs):
-            reflection_coefficients_dl[config, :] = np.mod(self.wavenumber * (np.sin(self.bs.incidence_angle) - np.sin(theta_s)) * self.ris.pos_els[:, 0], 2 * np.pi)
+            reflection_coefficients_dl[config, :] = 2 * np.pi * np.mod(self.wavenumber * (np.sin(self.bs.incidence_angle) - np.sin(theta_s)) * self.ris.pos_els[:, 0], 1)
+
+        # # LaTeX type definitions
+        # rc('font', **{'family': 'sans serif', 'serif': ['Computer Modern']})
+        # rc('text', usetex=True)
+        #
+        # lines = ['-', '--', ':', ':']
+        # markers = ['.', 'x', 's', 'v']
+        #
+        # fig, ax = plt.subplots(figsize=(3.15, 3))
+        #
+        # # Go through all configurations
+        # for config, theta_s in enumerate(self.ris.set_configs):
+        #     ax.plot(v_range / self.wavelength, np.rad2deg(local_surface_phase[config, :]), linewidth=1.5, linestyle=lines[config])
+        #     ax.plot(self.ris.pos_els[:, 0] / self.wavelength, np.rad2deg(reflection_coefficients_dl[config, :]), markers[config], linewidth=0.0)
+        #
+        # ax.set_xlabel(r'$x/\lambda$')
+        # ax.set_ylabel(r'local surface phase')
+        #
+        # ax.legend(loc='lower right')
+        # ax.set_yticks([0, 45, 90, 135, 180, 225, 270, 315, 360])
+        #
+        # plt.show()
+        #
+        # breakpoint()
 
         return reflection_coefficients_dl
 
     # Visualization methods
     def list_nodes(self, label=None):
-        """This method enlists the communication nodes.
-        """
+        """This method enlists the communication nodes."""
         if label is None:
             ls = '\n'.join(f'{i:2} {n}' for i, n in enumerate(self.node))
         elif label in common.node_labels:
@@ -277,7 +346,11 @@ class Box:
         # Reshape the reflection coefficient tensor from (S,N) -> (S,Na,Nb)
         reflection_coefficients_matrix = reflection_coefficients_flipped.reshape(self.ris.num_configs,
                                                                                  self.ris.num_els_v, self.ris.num_els_h)
+        # LaTeX type definitions
+        rc('font', **{'family': 'sans serif', 'serif': ['Computer Modern']})
+        rc('text', usetex=True)
 
+        # Open axes
         fig, ax = plt.subplots(nrows=decompose, ncols=decompose)
 
         # Go through all configurations
@@ -297,8 +370,12 @@ class Box:
                     # Get value in degrees
                     value_deg = np.round(np.rad2deg(reflection_coefficients_matrix[config][i, j]))
 
-                    # Print value
-                    ax[id_r][id_c].text(j, i, str(value_deg), va='center', ha='center')
+                    # Print value, note that j indexes the x-dimension (horizontal plot dimension)
+                    ax[id_r][id_c].text(j, i, str(value_deg), va='center', ha='center', color='black',
+                                        fontsize='x-small',fontweight='bold')
+
+                    ax[id_r][id_c].set_xlabel('$x$ [m]')
+                    ax[id_r][id_c].set_ylabel('$z$ [m]')
 
         plt.show()
 

environment/nodes.py
@@ -162,7 +162,7 @@ class RIS(Node):
         self.els_range = np.arange(self.num_els)
 
         # Each antenna element is a square of size wavelength/4
-        self.size_el = wavelength / 4
+        self.size_el = wavelength/4
 
         # Compute RIS sizes
         self.size_h = Nb * self.size_el  # horizontal size [m]

random-access.py
@@ -1,11 +1,15 @@
 from environment.box import Box
 import numpy as np
 
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
 import time
 
 ########################################
 # General parameters
 ########################################
+np.random.seed(42)
 
 # Square length
 ell = 100
@@ -46,7 +50,7 @@ channel_gains_dl, channel_gains_ul, phase_shifts_bs, phase_shifts_ue = box.get_c
 
 # Get DL reflection coefficients
 reflection_coefficients_dl = box.get_reflection_coefficients_dl
-#box.plot_reflection_coefficients(reflection_coefficients_dl)
+box.plot_reflection_coefficients(reflection_coefficients_dl)
 
 # Pilot selection
 pilot_selections = np.random.randint(0, taup, size=K).astype(int)
@@ -57,10 +61,11 @@ noise_ue = ((sigma2 / 2) ** (1 / 2)) * (np.random.randn(K) + 1j * np.random.rand
 # Compute total DL phase shifts of shape (S,K,N)
 phase_shifts_dl = reflection_coefficients_dl[:, np.newaxis, :] - phase_shifts_bs[np.newaxis, np.newaxis, :] - \
                   phase_shifts_ue[np.newaxis, :, :]  # TODO: Check if signs are correct
+phase_shifts_dl = 2 * np.pi * np.mod(phase_shifts_dl, 1)  # normalize (the closer to zero/2pi, the better)
 
 # Compute received DL beacon of shape (S,K)
 rx_dl_beacon_ue = np.sqrt(box.bs.max_pow * taup * channel_gains_dl)[np.newaxis, :] * \
-                  np.exp(1j * phase_shifts_dl.sum(axis=-1)) + noise_ue[np.newaxis, :]
+                  np.exp(1j * phase_shifts_dl).sum(axis=-1)  # + noise_ue[np.newaxis, :]
 
 # Compute magnitude of received DL beacon in each configuration for each UE
 magnitude_dl_beacon_ue = np.abs(rx_dl_beacon_ue)
@@ -69,14 +74,12 @@ magnitude_dl_beacon_ue = np.abs(rx_dl_beacon_ue)
 best_magnitude_config_ue = np.argmax(magnitude_dl_beacon_ue, axis=0)
 
 # Evaluates if this is making sense
-
-print("UE angles = ", np.round(np.rad2deg(np.arctan(box.ue.pos[:, 1] / box.ue.pos[:, 0])), 2))
-print("Best configs = ", best_magnitude_config_ue)
-
-for cc, config in enumerate(box.ris.set_configs):
-    print("RIS config " + str(cc) + " is centered at = ", np.round(np.rad2deg(box.ris.set_configs[cc]), 2))
-
-breakpoint()
+for k in range(K):
+    print("(best config, UE angle) = (" + str(np.round(np.rad2deg(box.ris.set_configs[best_magnitude_config_ue[k]]), 2))
+          + "," +
+          str(np.round(np.rad2deg(np.arctan(box.ue.pos[k, 1] / box.ue.pos[k, 0])), 2))
+          + ")"
+          )
 
 box.plot_scenario()
 
@@ -95,6 +98,7 @@ num_setups = 100
 
 # Prepare to count number of collisions
 num_collisions = np.zeros((num_inactive_ue_range.size, num_setups))
+num_collisions_ris_assisted = np.zeros((num_inactive_ue_range.size, num_setups))
 
 #####
 
@@ -129,12 +133,13 @@ for ii, num_inactive_ue in enumerate(num_inactive_ue_range):
 
         # Compute total DL phase shifts of shape (S,Ka,N)
         phase_shifts_dl = reflection_coefficients_dl[:, np.newaxis, :] - phase_shifts_bs[np.newaxis, np.newaxis, :] - \
-                          phase_shifts_ue[np.newaxis, :, :]  # TODO: Check if signs are correct
-        phase_shifts_dl = np.mod(phase_shifts_dl, 2 * np.pi)
+                          phase_shifts_ue[np.newaxis, :, :]
+
+        phase_shifts_dl = 2 * np.pi * np.mod(phase_shifts_dl, 1)
 
         # Compute received DL beacon of shape (S,K)
         rx_dl_beacon_ue = np.sqrt(box.bs.max_pow * taup * channel_gains_dl)[np.newaxis, :] * \
-                          np.exp(1j * phase_shifts_dl.sum(axis=-1)) + noise_ue[np.newaxis, :]
+                          (np.exp(1j * phase_shifts_dl)).sum(axis=-1) + noise_ue[np.newaxis, :]
 
         # Compute magnitude of received DL beacon in each configuration for each UE
         magnitude_dl_beacon_ue = np.abs(rx_dl_beacon_ue)
@@ -142,7 +147,50 @@ for ii, num_inactive_ue in enumerate(num_inactive_ue_range):
         # Find the best configuration for each UE in terms of best magnitude
         best_magnitude_config_ue = np.argmax(magnitude_dl_beacon_ue, axis=0)
 
-        # Count number of collisions
+        # Combine pilot selection and best configs
+        temp = [(x, y) for (x, y) in zip(pilot_selections, best_magnitude_config_ue)]
 
+        magnitude_based_ue = np.empty(len(temp), dtype=object)
+        magnitude_based_ue = temp
 
-        breakpoint()
+        del temp
+
+        _, collision_counting_ris_assisted = np.unique(magnitude_based_ue, return_counts=True, axis=0)
+
+        # Count number of collisions: same pilot AND best configuration. The -1 is to disregard the non-collision case
+        num_collisions_ris_assisted[ii, ss] = (collision_counting_ris_assisted - 1).sum()
+
+        # Count number of collisions w/o RIS assistance
+        _, collision_counting = np.unique(pilot_selections, return_counts=True)
+        num_collisions[ii, ss] = (collision_counting - 1).sum()
+
+########################################
+# Plot
+########################################
+
+# LaTeX type definitions
+rc('font', **{'family': 'sans serif', 'serif': ['Computer Modern']})
+rc('text', usetex=True)
+
+# Open axes
+fig, ax = plt.subplots()
+
+ax.plot(num_inactive_ue_range, num_collisions.mean(axis=-1), label='Classical')
+ax.plot(num_inactive_ue_range, num_collisions_ris_assisted.mean(axis=-1), label='RIS-assisted: $S =' + str(S) + '$')
+
+# Set axis
+ax.set_xlabel('number of inactive UEs')
+ax.set_ylabel('average number of collisions')
+
+# # limits
+# ax.set_ylim(ymin=-self.ell0 / 2)
+
+# Legend
+ax.legend()
+# handles, labels = plt.gca().get_legend_handles_labels()
+# by_label = OrderedDict(zip(labels, handles))
+# ax.legend(by_label.values(), by_label.keys())
+
+# Finally
+plt.grid(color='#E9E9E9', linestyle='--', linewidth=0.5)
+plt.show(block=False)